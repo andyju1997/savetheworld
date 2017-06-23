485e63be3ce30736fa3c74c96f80cc0da20124ce
Andy Ju <andyju1997@gmail.com>
2017-06-22 20:56:06 -0700

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by Andy Ju on 6/22/17.
+//  Copyright © 2017 Andy Ju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
